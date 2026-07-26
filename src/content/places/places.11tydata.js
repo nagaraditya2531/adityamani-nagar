157e2a0cdef35fb9ea7dd3af3e8be3d173f9094b
nagaraditya2531 <nagaraditya2531@gmail.com>
2026-07-27 00:53:13 +0100

    Delete src/content/places directory

--- src/content/places/places.11tydata.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  layout: "place.njk",
-  visibility: "public",
-  eleventyComputed: {
-    permalink: (data) =>
-      data.visibility === "draft" ? false : `/places/${data.page.fileSlug}/`
-  }
-};
